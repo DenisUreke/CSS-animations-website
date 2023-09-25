79b40f0bfb9d44c4aba64cc7b6ea02a87220b2db
Denis Ureke <70695931+DenisUreke@users.noreply.github.com>
2023-09-25 21:29:27 +0200

    database is up

--- app.js
+++ app.js
@@ -1,61 +1,78 @@
-const express = require('express') // loads the express package
-const { engine } = require('express-handlebars'); // loads handlebars for Express
-const port = 8080 // defines the port
-const app = express() // creates the Express application
+const express = require('express');
+const { engine } = require('express-handlebars');
 
-// defines handlebars engine
+const app = express();
+const port = 8080;
+
+// Database setup
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('database.db');
+
+// Handlebars setup
 app.engine('handlebars', engine());
-// defines the view engine to be handlebars
 app.set('view engine', 'handlebars');
-// defines the views directory
 app.set('views', './views');
 
-// define static directory "public" to access css/ and img/
-app.use(express.static('public'))
+// Static file setup
+app.use(express.static('public'));
 
-// MODEL (DATA)
+// Model (data)
 const humans = [
-    {"id": "0", "name": "Jerome"}, 
+    {"id": "0", "name": "Jerome"},
     {"id": "1", "name": "Mira"},
-    {"id": "2", "name": "Linus"}, 
-    {"id": "3", "name": "Susanne"}, 
-    {"id": "4", "name": "Jasmin"}, 
-]
+    {"id": "2", "name": "Linus"},
+    {"id": "3", "name": "Susanne"},
+    {"id": "4", "name": "Jasmin"},
+];
+
+// Routes
+app.get('/', (request, response) => {
+    response.render('home', { layout: 'adminLayout' });
+});
+
+app.get('/humans', (request, response) => {
+    const model = { listHumans: humans };
+    response.render('humans.handlebars', model);
+});
+
+app.get('/humans/1', (request, response) => {
+    const model = humans[1];
+    response.render('human.handlebars', {});
+});
+
+app.get('/forum/:id', (req, res) => {
+  const { id } = req.params;
+  console.log(`Received request for forum with ID: ${id}`);
+
+  // Query the database
+  db.get('SELECT * FROM User WHERE id = ?', [id], (err, row) => {
+      if (err) {
+          console.error(`Error fetching data for ID: ${id}. Error: ${err.message}`);
+          return res.status(500).json({ error: 'Server error' });
+      }
+
+      if (row) {
+          console.log(`Successfully fetched data for ID: ${id}:`, row);
+          return res.status(200).json(row);
+      } else {
+          console.warn(`No data found for ID: ${id}`);
+          return res.status(404).json({ error: 'Not found' });
+      }
+  });
+})
+
+// 404 route
+app.use((req, res) => {
+    res.status(404).render('404.handlebars');
+});
+
+// Server start
+app.listen(port, () => {
+    console.log(`Server running and listening on port ${port}...`);
+});
 
 // CONTROLLER (THE BOSS)
 // defines route "/"
 /*app.get('/', function(request, response){
   response.render('layouts/adminLayout.handlebars')
 }) ***/
-
-app.get('/', function(request, response){
-response.render('home', { layout: 'adminLayout' });
-})
-
-// defines route "/humans"
-app.get('/humans', function(request, response){
-  const model = { listHumans: humans } // defines the model
-  // in the next line, you should send the abovedefined 
-  // model to the page and not an empty object {}...
-  response.render('humans.handlebars', model)
-})
-
-
-// defines route "/humans/1"
-app.get('/humans/1', function(request, response){
-  const model = humans[1] // defines the model
-  // in the next line, you should send the abovedefined 
-  // model to the page and not an empty object {}...
-  response.render('human.handlebars', {})
-})
-
-// defines the final default route 404 NOT FOUND
-app.use(function(req,res){
-  res.status(404).render('404.handlebars');
-});
-
-// runs the app and listens to the port
-app.listen(port, () => {
-    console.log(`Server running and listening on port ${port}...`)
-})
-
